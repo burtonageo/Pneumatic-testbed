bc9e0cf3f4a3ee0cd5fb5b723dfc726f27205655
George Burton <burtonageo@gmail.com>
2014-08-07 23:39:55 +0100

    Now compatible with latest Pneumatic

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,7 +7,7 @@
 auto main(int argc, const char** argv) -> int {
   Pneumatic::Graphics::Window win("testing", 800, 600, 80, 60);
 
-  win.init().ifNotOk([](const std::string& error) {
+  win.init().onError([](const std::string& error) {
                        std::cout << error << std::endl;
                        exit(1);
                      });
