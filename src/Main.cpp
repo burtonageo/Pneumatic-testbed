78e6d2672ee865c0ccb4ca61fdf6a8754de44af6
George Burton <burtonageo@gmail.com>
2014-08-14 12:36:10 +0100

    Main now uses Color class to set window bg color

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <iostream>
 
+#include "pneu/graphics/Color.hpp"
 #include "pneu/graphics/Window.hpp"
 #include "pneu/graphics/Renderer.hpp"
 #include "pneu/core/MethodResult.hpp"
@@ -18,7 +19,7 @@ auto main(int argc, const char** argv) -> int {
                         });
 
   // set background to a nice shade of blue
-  window.getRenderer().setBackgroundColor(glm::vec3(0.2f, 0.3f, 0.7f));
+  window.getRenderer().setBackgroundColor(pneu::graphics::Color3(0.2f, 0.3f, 0.7f));
 
   while (window.isRunning()) {
     window.pollEvents();
